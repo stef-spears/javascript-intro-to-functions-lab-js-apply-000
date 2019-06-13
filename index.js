211a70b993805100dd797d13d7a9d4d7f31c0d2b
Stef Spears <spears.sj87@gmail.com>
2019-06-13 21:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-/*describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})*/
 
 function logShout(string) {
   console.log(string.toUpperCase());
